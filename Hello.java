c6ccc79e01cd42f3a5f4e87f96f0edeeffc62059
SalC124 <saltcal124@gmail.com>
2024-02-01 15:44:49 -0500

    Badabing Badaboom

---
+++ Hello.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        try(Scanner input = new Scanner(System.in);) {
+            System.out.println("Enter your name: ");
+            String name = input.nextLine();
+            System.out.println("Hello " + name + "!");
+            input.close();
+        }
+    }
+}
