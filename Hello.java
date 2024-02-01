f6d8f580641c401a7f0f8a43d18f6ff36f08113c
SalC124 <saltcal124@gmail.com>
2024-02-01 15:49:07 -0500

    Salutations HAHAHA

--- Hello.java
+++ Hello.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
-        try(Scanner input = new Scanner(System.in);) {
+        try(Scanner input = new Scanner(System.in)) {
             System.out.println("Enter your name: ");
             String name = input.nextLine();
-            System.out.println("Hello " + name + "!");
-            input.close();
+            System.out.println("Salutations " + name + "!");
         }
     }
 }
