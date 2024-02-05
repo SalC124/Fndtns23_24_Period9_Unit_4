6ce0f08194eb4a2fbd06882fa8ef41a988777c5a
SalC124 <saltcal124@gmail.com>
2024-02-05 15:50:25 -0500

    Day 2

--- BeanCounter.java
+++ BeanCounter.java
@@ -8,9 +8,32 @@ public class BeanCounter {
         this.name = name;
         this.beanCount = beanCount;
     }
+    // overloaded constructor
+    public BeanCounter(String name) {
+        this.name = name;
+        beanCount = 0;
+    }
 
     // toString method
     public String toString() {
         return "name: " + name + " Bean Count: " + beanCount;
+    }
+
+    // getters
+    public String getName() {
+        return name;
+    }
+    public int getBeanCount() {
+        return beanCount;
+    }
+
+    // mutators
+    public void count() {
+        beanCount++;
+    }
+
+    // setters
+    public void setName(String newName) {
+        name = newName;
     }
 }
--- BeanCounterDemo.java
+++ BeanCounterDemo.java
@@ -7,5 +7,23 @@ public class BeanCounterDemo {
         System.out.println(counter1.toString() + "explicit");
         // implicit use of toString() method
         System.out.println(counter1 + "implicit");
+
+        BeanCounter counter2 = new BeanCounter("Jane Doe");
+        System.out.println("Counter2: " + counter2);
+
+        System.out.println("Name of the Counter2: " + counter2.getName());
+        System.out.println("Name of the Counter1: " + counter1.getName());
+
+        System.out.println("Beans of the Counter2: " + counter2.getBeanCount());
+
+        for(int i = 0; i < 5; i++) {
+            counter2.count();
+        }
+        System.out.println("The count for counter2: " + counter2.getBeanCount());
+
+        // set the counter 1's name to Jack
+        counter1.setName("Jack");
+
+        System.out.println("The updated name of counter1: " + counter1.getName());
     }
 }
