0ff001d15e171747ad1000c7b92a1dd92236345f
SalC124 <saltcal124@gmail.com>
2024-03-06 08:04:41 -0500

    Day "5"

--- Circle2d.java
+++ Circle2d.java
@@ -26,17 +26,31 @@ public class Circle2d {
     public double getRadius() {
         return radius;
     }
+    public double getArea() {
+        return Math.PI * Math.pow(radius,2);
+    }
+    public double getCircumference() {
+        return (2 * radius * Math.PI);
+    }
 
     // methods
     public void translate(double dx, double dy) {
         x += dx;
-        y += dy; 
+        y += dy;
     }
     public void expand(double amt) {
         radius += amt;
     }
+    public double distance(Circle2d c2) {
+        return Math.pow(Math.pow(Math.abs(x - c2.getX()),2) + Math.pow(Math.abs(y - c2.getY()),2),0.5);
+    }
+    public boolean contains(double x, double y) {
+        return Math.pow(Math.pow(Math.abs(this.x - x), 2) + Math.pow(Math.abs(this.y - y), 2), 0.5) == radius;
+    }
 
-
-
+// toString (yes, it get's its own section)
+    public String toString() {
+        return "Circle: (" + x + ", " + y + "); Radius: " + radius;
+    }
 
 }
